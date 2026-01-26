2b4705d7d25200958d833bd6c2633504f11dcf1a
Sheddy <okundashedrack@gmail.com>
2026-01-26 13:31:09 +0300

    updated the package-lock.json

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d565866'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.87qc5gebbs8"
+    "revision": "0.5e4a75dcvpo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
